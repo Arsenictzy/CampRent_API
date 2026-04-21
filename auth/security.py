850afab23490947dd355b1cdd639a9418bec5c73
Arsenictzy <kevinsoamkila@gmail.com>
2026-04-21 23:06:14 +0800

    final

--- auth/security.py
+++ auth/security.py
@@ -4,7 +4,10 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-SECRET_KEY = "KEVIN_SECRET_CAMP" 
+SECRET_KEY = "KEVIN_SECRET_CAMP"
+ALGORITHM = "HS256"  
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 pwd_context = CryptContext(
@@ -14,23 +17,18 @@ pwd_context = CryptContext(
 )
 
 def hash_password(password: str):
-    """Mengubah password plain text menjadi hash yang aman."""
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
-    """Memverifikasi apakah password input cocok dengan hash di database."""
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict):
-    """Membuat token JWT dengan masa berlaku 30 menit[cite: 81]."""
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=30)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    """Validasi token JWT untuk melindungi endpoint yang membutuhkan proteksi[cite: 32]."""
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
